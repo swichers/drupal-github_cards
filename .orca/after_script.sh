593ecc8afb6c49f8c452a4bba48fd903a67f777b
Steven Wichers <me@stevenwichers.com>
2020-07-23 01:40:55 -0700

    Rename env variable to match latest update.

--- .orca/after_script.sh
+++ .orca/after_script.sh
@@ -12,7 +12,7 @@
 cd "$(dirname "$0")" || exit
 source ../../orca/bin/travis/_includes.sh
 
-if [[ "${ORCA_COLLECT_COVERAGE}" == "TRUE" ]]; then
+if [[ "${ORCA_COVERAGE_COLLECT}" == "TRUE" ]]; then
   composer -d"${TRAVIS_BUILD_DIR}" require --dev 'codacy/coverage:dev-master'
   "${TRAVIS_BUILD_DIR}/vendor/bin/codacycoverage" clover --git-commit ${TRAVIS_COMMIT} "${ORCA_COVERAGE_PATH}/clover.xml"
 fi
