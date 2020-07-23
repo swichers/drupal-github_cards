de0c7c906bd31ff3a47a72eec5afd45a5f46e5b6
Steven Wichers <me@stevenwichers.com>
2020-07-23 00:43:11 -0700

    Use custom fork of ORCA to add coverage back in.

--- .orca/after_script.sh
+++ .orca/after_script.sh
@@ -12,9 +12,7 @@
 cd "$(dirname "$0")" || exit
 source ../../orca/bin/travis/_includes.sh
 
-
-
-if [[ "${ORCA_JOB}" == "ISOLATED_RECOMMENDED" ]]; then
+if [[ "${ORCA_COLLECT_COVERAGE}" == "TRUE" ]]; then
   composer -d"${TRAVIS_BUILD_DIR}" require --dev 'codacy/coverage:dev-master'
   "${TRAVIS_BUILD_DIR}/vendor/bin/codacycoverage" clover --git-commit ${TRAVIS_COMMIT} "${ORCA_ROOT}/var/coverage/github_cards/clover.xml"
 fi
