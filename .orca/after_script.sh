3b34c9a854b661276ca9a060fb857308909aa848
Steven Wichers <me@stevenwichers.com>
2020-07-23 01:13:26 -0700

    Set a coverage path directly.

--- .orca/after_script.sh
+++ .orca/after_script.sh
@@ -14,5 +14,5 @@ source ../../orca/bin/travis/_includes.sh
 
 if [[ "${ORCA_COLLECT_COVERAGE}" == "TRUE" ]]; then
   composer -d"${TRAVIS_BUILD_DIR}" require --dev 'codacy/coverage:dev-master'
-  "${TRAVIS_BUILD_DIR}/vendor/bin/codacycoverage" clover --git-commit ${TRAVIS_COMMIT} "${ORCA_ROOT}/var/coverage/github_cards/clover.xml"
+  "${TRAVIS_BUILD_DIR}/vendor/bin/codacycoverage" clover --git-commit ${TRAVIS_COMMIT} "${ORCA_COVERAGE_PATH}/clover.xml"
 fi
